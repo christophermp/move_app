edab6d0e977ce53d1b6c6f820e2a5c0bae5b7922
Christopher Meldre Pettersen <meldre@outlook.com>
2022-01-23 19:35:12 +0100

    Initial

---
+++ lib/theme/palette.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/rendering.dart';
+
+class Palette {
+  static const Color almostBlack = Color(0xff040B13);
+  static const Color red500 = Color(0xffE82139);
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,1 @@
+// TODO: We will add testing later
